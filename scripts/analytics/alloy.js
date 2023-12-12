b69b63c7e12e260c8f63798d6734661358c2b7ca
Antoniu Neacsu <aneacsu@adobe.com>
2023-12-12 15:25:11 +0200

    edge

--- scripts/analytics/alloy.js
+++ scripts/analytics/alloy.js
@@ -10493,7 +10493,7 @@
   OF ANY KIND, either express or implied. See the License for the specific language
   governing permissions and limitations under the License.
   */
-  var EDGE = "edge.adobedc.net";
+  var EDGE = "beta.adobedc.net";
   var ID_THIRD_PARTY = "adobedc.demdex.net";
 
   /*
